6f3af73d24d84e75ede36dcfa57857e65058259f
Yonghyeon Kwon <dydgus9952@naver.com>
2023-11-26 15:23:58 +0900

    kyh 231126 add: main page

---
+++ src/pages/components/MyCarousel.tsx
@@ -0,0 +1,29 @@
+import { Image } from 'react-bootstrap';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+
+interface Image {
+    src: string;
+    alt: string;
+    caption: string;
+  }
+  
+  interface MyCarouselProps {
+    images: Image[];
+  }
+
+const MyCarousel: React.FC<MyCarouselProps> = ({ images }) => {
+  return (
+    <div>
+        <Carousel className='carousel'>
+        {images.map((image, index) => (
+            <div className="carouselItem" key={index}>
+                <Image src={image.src} alt={image.alt} width={100} height={80}/>
+            </div>
+        ))}
+        </Carousel>
+    </div>
+  );
+};
+
+export default MyCarousel;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <></>
+    <>
+    <h1>dkssudgktpdya</h1>
+    </>
   )
 }
