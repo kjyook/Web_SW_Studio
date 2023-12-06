f70010b5bea318a033be0422fceb935669b85620
kjyook <106497893+kjyook@users.noreply.github.com>
2023-12-06 21:31:28 +0900

    routing guard 작성. 근데 _app.tsx 에는 적용이 안됨

--- src/models/RoutingGuard.tsx
+++ src/models/RoutingGuard.tsx
@@ -3,9 +3,11 @@ import { useNavigation } from "@/hooks/useNavigation";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { useEffect } from "react";
 
-export const RouteController = () => {
+export const RoutingGuard = () => {
     const { data: user } = useCurrentUser();
     const { navigateTo, getCurrentPath } = useNavigation();
+
+    console.log(user);
 
     //여기에 보호될 페이지들 전부 작성하시면 됩니다.
     const protectedPages = ['/'];
