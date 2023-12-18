42f27fade42dbfce599e894b08aa2e07d5d3f2bf
kjyook <kjyook01@gmail.com>
2023-12-18 12:55:29 +0900

    오타 수정, 잠시 모델 삭제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,13 @@
-import { RoutingGuard } from '@/models/RoutingGuard'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react';
-import { useNavigation } from '@/hooks/useNavigation';
 
 export const App = ({ Component, pageProps }: AppProps) => {
-  /* const { router } = useNavigation();
 
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      RoutingGuard();
-      console.log('Route is changing to', url);
-    };
-
-    router.events.on('routeChangeStart', handleRouteChange);
-
-    return () => {
-      router.events.off('routeChangeStart', handleRouteChange);
-    };
-  }, [router.events]); */
-
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
